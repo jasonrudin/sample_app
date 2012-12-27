346c9ff8a61dce416f94e82909d2c483957cee66
Jason Rudin <j.b.rudin@gmail.com>
2012-12-26 21:36:31 -0500

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
+  def show 
+  	@user = User.find(params[:id])
+  end
   def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+      flash[:success] = "Welcome to Baby Twitter, have fun and don't try anything crazy!"
+      redirect_to @user
+  	else
+  		render 'new'
+  	end
   end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Jason Rudin"
+    email    "jrudin@wharton.upenn.edu"
+    password "tallguy11192"
+    password_confirmation "tallguy11192"
+  end
+end
