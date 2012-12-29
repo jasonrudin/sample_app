d9adb5a62493ca8650ad79689ca9536ca9243a0c
Jason Rudin <j.b.rudin@gmail.com>
2012-12-28 22:41:24 -0500

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,13 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
+#  remember_token  :string(255)
 #
 
 require 'spec_helper'
@@ -26,6 +28,7 @@ describe User do
   it {should respond_to{:password}}
   it {should respond_to{:password_confirmation}}
   it {should respond_to{:authenticate}}
+  it {should respond_to{:remember_token}}
   it {should be_valid}
 
   describe "when name is not present" do
@@ -114,6 +117,11 @@ end
 describe "with a password that's too short" do
   before { @user.password = @user.password_confirmation = "a" * 5 }
   it { should be_invalid }
+end
+
+describe "remember token" do
+  before{@user.save}
+  its(:remember_token){should be_blank}
 end
 
 
